43e575072b6861c7476b020f01d4218cb3368a8a
nekotyan2d <dmitryrychazhkov2019@gmail.com>
2026-04-18 09:41:40 +0300

    feat: add github pages deployment

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { createRouter, createWebHistory } from "vue-router";
 const pinia = createPinia();
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory("/wikilink/"),
     routes: [
         {
             path: "/",
